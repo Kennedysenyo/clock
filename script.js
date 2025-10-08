a3b37b519bcf77787eaef313d7e7758e19529063
kennedysenyo <kennedysenyo@gmail.com>
2025-10-08 15:05:34 +0000

    add clock functionality

---
+++ script.js
@@ -0,0 +1,23 @@
+const hourHand = document.querySelector(".hour");
+const minuteHand = document.querySelector(".minute");
+const secondHand = document.querySelector(".second");
+
+const updateClock = () => {
+  const currentDate = new Date();
+  setInterval(updateClock, 1000);
+  const hour = currentDate.getHours();
+  const minute = currentDate.getMinutes();
+  const second = currentDate.getSeconds();
+  // console.log(hour, minute, second);
+
+  const hourDeg = (hour / 12) * 360;
+  hourHand.style.transform = `rotate(${hourDeg}deg)`;
+
+  const minuteDeg = (minute / 60) * 360;
+  minuteHand.style.transform = `rotate(${minuteDeg}deg)`;
+
+  const secondDeg = (second / 60) * 360;
+  secondHand.style.transform = `rotate(${secondDeg}deg)`;
+};
+
+updateClock();
